5c8dfa79e77d64b810f1efbdd7d3a7049e9b01b3
ross3834 <rossandus@hotmail.com>
2017-09-26 03:18:58 -0700

    Multiple Changes:
    		The solver can now find naked subsets
    		and remove the appropriate candidates
    		based on this.
    
    		basis for x-wing solving has been added.
    
    		Solver now generates a table of all posibilities
    		only once at start. It then eliminates candidates
    		rather than regenerating after each step.
    		This is useful because it will help to make
    		the output look less convoluted.

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -122,21 +122,57 @@ def Solver(filename: str):
 
 	#: Main loop to solve puzzle:
 	pass_number = 1
+	past_pencil = []
+
+	#: Generate the pencil
+	pencil = [[[i for i in range(1, 10)] for i in range(9)] for i in range(9)]
+
+	#pencil = [[[0] for i in range(9)] for i in range(9)]
+	#for blank_index, blank in enumerate(blanks):
+	#	row = get_row(blank[0], puzzle)
+	#	col = get_col(blank[1], puzzle)
+	#	box = get_box(blank[0] // 3, blank[1] // 3, puzzle)  #: 0, 2 => 0, 0/ 5, 5 => 1, 1
+
+	#	for i in range(1,10):
+	#		if i not in row and i not in col and i not in box:
+	#			if pencil[blank[0]][blank[1]] == [0]:
+	#				pencil[blank[0]][blank[1]] = []
+	#			pencil[blank[0]][blank[1]].append(i)
+
 	while len(blanks) != 0:
 		print("Pass number: " + str(pass_number))
 
-		#: Find possible solutions.
-		pencil = [[[0] for i in range(9)] for i in range(9)]
-		for blank_index, blank in enumerate(blanks):
-			row = get_row(blank[0], puzzle)
-			col = get_col(blank[1], puzzle)
-			box = get_box(blank[0] // 3, blank[1] // 3, puzzle)  #: 0, 2 => 0, 0/ 5, 5 => 1, 1
+		past_pencil = list(pencil)
 
-			for i in range(1,10):
-				if i not in row and i not in col and i not in box:
-					if pencil[blank[0]][blank[1]] == [0]:
-						pencil[blank[0]][blank[1]] = []
-					pencil[blank[0]][blank[1]].append(i)
+
+		#: If a cell is not empty, remove all possibilities from this cell.
+		for r_index, row in enumerate(puzzle):
+			for c_index, cell in enumerate(row):
+				if cell != 0:
+					pencil[r_index][c_index] = [0]
+
+		#: Remove candidates if a number has been entered into a cell by row, col, and box:
+		for subset in range(9):
+			s_row = get_row(subset, puzzle)
+			s_col = get_col(subset, puzzle)
+			s_box = get_box(subset // 3, subset % 3, puzzle)
+
+			for i in range(1, 10):
+				if i in s_row:
+					p_row = get_row(subset, pencil)
+					for p_cell in p_row:
+						if i in p_cell:
+							p_cell.remove(i)
+				if i in s_col:
+					p_col = get_col(subset, pencil)
+					for p_cell in p_col:
+						if i in p_cell:
+							p_cell.remove(i)
+				if i in s_box:
+					p_box = get_box(subset // 3, subset % 3, pencil)
+					for p_cell in p_box:
+						if i in p_cell:
+							p_cell.remove(i)
 
 		#: Remove Candidates from pencil with Block and column/Row Interaction
 		#: If within a box, there are two+ numbers than exist in a line (row or col)
@@ -149,7 +185,6 @@ def Solver(filename: str):
 				#: Remove candidates from rows
 				for inner_row in range(3):
 					row_within_box = box[inner_row * 3: (inner_row * 3) + 3]
-					#print(row_within_box)
 					for i in range(1, 10):
 						count = 0
 						double_count = 0
@@ -166,7 +201,7 @@ def Solver(filename: str):
 									if i in removal_spos:
 										removal_spos.remove(i)
 										print("Removed " + str(i) + " as a possibility from row " + str((inner_row + ((box_row) * 3) + 1)) +
-										      " and col " + str(removal_index + 1))
+										" and col " + str(removal_index + 1))
 
 				#: Remove candidates from columns
 				for inner_col in range(3):
@@ -187,11 +222,89 @@ def Solver(filename: str):
 									if i in removal_spos:
 										removal_spos.remove(i)
 										print("Removed " + str(i) + " as a possibility from row " + str(removal_index + 1) +
-										      " and col " + str(inner_col + ((box_col) * 3)))
+										" and col " + str(inner_col + ((box_col) * 3)))
 
 		#: Remove candidates throgh the X-Wing and SwordFish techniques
 		#: Check for x-wing style board in cols
-		#x_wing_connections = find_xwing_patterns(pencil) #: Returns data structure with [[[value to remove, row], [], []], [[value to remove, col], [], []]]
+		x_wing_connections = find_xwing_patterns(pencil) #: Returns a list with a tuple of the value and row to remove.
+		if x_wing_connections is not None:
+			print("this was actually used!!!")
+			assert("this is within x_wing_connections area" == 0)
+			for tup in x_wing_connections:
+				row = get_row(tup[1], pencil)
+				for p_set in row:
+					if tup[0] in p_set:
+						p_set.remove(tup[0])
+
+		#: Remove candidated based on naked sets
+		for subset in range(9):
+			p_row = get_row(subset, pencil)
+			p_col = get_col(subset, pencil)
+			p_box = get_box(subset // 3, subset % 3, pencil)
+
+			#: Find naked sets within Rows
+			for c_set in p_row:
+				type = len(c_set)
+				in_naked = []
+				if type >= 2:
+					for index, comparison in enumerate(p_row):
+						#: If comparison is not the set, and comparison is a subset of set
+
+						if set(comparison) <= set(c_set) and len(comparison) >= 2:
+							in_naked.append(index)
+
+					if len(in_naked) >= type and len(in_naked) != len([x for x in p_row if x != [0]]):
+						for extranious in [value for value in range(9) if value not in in_naked]:
+							#: Generate a new list without the values in c_set that are being removed.
+							new_set = [new_set for new_set in p_row[extranious] if new_set not in c_set]
+
+							if new_set != p_row[extranious]:
+								p_row[extranious] = new_set
+								print("Removed " + str(c_set) + " from (" + str(subset) + ", " + str(extranious) + ") Using naked subsets from rows")
+
+			#: Find naked sets within Cols
+			for c_set in p_col:
+				type = len(c_set)
+				in_naked = []
+				if type >= 2:
+					for index, comparison in enumerate(p_col):
+						#: If comparison is not the set, and comparison is a subset of set
+
+						if set(comparison) <= set(c_set) and len(comparison) >= 2:
+							in_naked.append(index)
+
+					if len(in_naked) >= type and len(in_naked) != len([x for x in p_col if x != [0]]):
+						for extranious in [value for value in range(9) if value not in in_naked]:
+							#: Generate a new list without the values in c_set that are being removed.
+							new_set = [new_set for new_set in p_col[extranious] if new_set not in c_set]
+
+							if new_set != p_col[extranious]:
+								p_col[extranious] = new_set
+								print("Removed " + str(c_set) + " from (" + str(extranious) + ", " + str(subset) + ") Using naked subsets from cols")
+
+			#: Find naked sets within Boxes
+			for c_set in p_box:
+				type = len(c_set)
+				in_naked = []
+				if type >= 2:
+					for index, comparison in enumerate(p_box):
+						#: If comparison is not the set, and comparison is a subset of set
+
+						if set(comparison) <= set(c_set) and len(comparison) >= 2:
+							in_naked.append(index)
+
+					if len(in_naked) >= type and len(in_naked) != len([x for x in p_box if x != [0]]):
+						for extranious in [value for value in range(9) if value not in in_naked]:
+							#: Generate a new list without the values in c_set that are being removed.
+							new_set = [new_set for new_set in p_col[extranious] if new_set not in c_set]
+
+							if new_set != p_box[extranious]:
+								p_box[extranious] = new_set
+								print("Removed " + str(c_set) + " from box (" + str(subset // 3) + ", " + str(subset % 3) +
+									") and cell (" + str(extranious // 3) + ", " + str(extranious % 3) +
+									") Using naked subsets from boxes")
+
+#------------------------------------------------- CODE BELOW THIS LINE IS USED TO FILL CELLS. CODE ABODE IS TO REMOVE CANDIDATED.-------------------------------------------------------------#
 
 		#: Find cells with only one posibility
 		for prow_index, pencilled_row in enumerate(pencil):
@@ -256,9 +369,11 @@ def Solver(filename: str):
 			for blank in mark:
 				blanks.remove(blank)
 		else:
+		#if len(mark) == 0 and past_pencil == pencil:
 			print("pencil: ")
-			for item in pencil:
-				print(str(item) + '\t')
+			print_puzzle(pencil)
+			#for item in pencil:
+				#print(str(item) + '\t')
 
 			#print("blanks: ")
 			#for item in blanks:
@@ -278,33 +393,63 @@ def Solver(filename: str):
 		pass_number += 1
 	print("The puzzle has been sucessfully solved after " + str(pass_number - 1) + " passes.")
 
-def find_xwing_patterns(pencill: str): #: Returns data structure with [[[value to remove, row], [], []], [[value to remove, col], [], []]]
-	removes = [[], []]
-	for i in range(9):
+def find_xwing_patterns(pencil: str) -> list: #: Returns a list of tuples containing the values to remove from rows. 
+	remove = []
+	mark_for_removal = []
+	for i in range(1, 10):
 		for x in range(9):
 			for y in range(9):
-				if x not in removes[0] and y not in removes[1]:
+				if (i, x) not in remove:
 					by_row = True
-					while get_next_connection(i, (x, y), pencil, by_row) is not [True, x, y]:
+					next_con = get_next_connection(i, (x, y), pencil, by_row)
+#					print(str(i) + ": ", x, y, sep=', ', end=':-------------------------------\n')
+					while next is not [True, x, y]:
 						by_row = not by_row
-						#if 
+						if next_con[0] is False:
+							mark_for_removal.clear()
+							break
+						else:
+							mark_for_removal.append((i, next_con[1]))
+
+						next_con = get_next_connection(i, (next_con[1], next_con[2]), pencil, by_row)
+#					print('\n')
+					if len(mark_for_removal) != 0:
+						for mark in mark_for_removal:
+							remove.append(tuple(mark))
+
 def get_next_connection(value: int, coordinate: tuple, pencil: str, return_row: bool) -> list: #: [IS CONNECTED, connection x, connection y]
+	i = value
 	is_connected = False
 	connection_x = -1
 	connection_y = -1
 	cord_row = get_row(coordinate[0], pencil)
 	cord_col = get_col(coordinate[1], pencil)
+	built_row = []
+	built_col = []
 
-	if cord_row.count(i) == 2 and cord_col.count(i) == 2:
-		is_connected = True
-		if return_row:
-			connection_x = cord_row.index(i) if cord_row.index(i) != coordinate[0] else cord_row.index(i, 1)
-			connection_y = coordinate[1]
-		else:
-			connection_x = coordinate[0]
-			connection_y = cord_col.index(i) if cord_col.index(i) != coordinate[1] else cord_col.index(i, 1)
+	for p_set in cord_row:
+		for value in p_set:
+			if value == i:
+				built_row.append([value])
 
-	return [is_connected, connectionx, connection_y]
+	for p_set in cord_col:
+		for value in p_set:
+			if value == i:
+				built_col.append([value])
+
+	if (return_row and [i] in built_row) or (not return_row and [i] in built_col):
+		if (return_row and built_row.count([i]) == 2) or (not return_row and built_col.count([i]) == 2):
+			is_connected = True
+			if return_row:
+				connection_x = built_row.index([i]) if built_row.index([i]) != coordinate[0] else built_row.index([i], 1)
+				connection_y = coordinate[1]
+			else:
+				connection_x = coordinate[0]
+				connection_y = built_col.index([i]) if built_col.index([i]) != coordinate[1] else built_col.index([i], 1)
+#	if connection_x != -1:
+#		print(connection_x, connection_y, sep=', ')
+	return [is_connected, connection_x, connection_y]
+
 
 if __name__ == "__main__":
 	Solver(argv[1])
