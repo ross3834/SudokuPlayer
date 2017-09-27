c88d819613b47f718fbcdd70c5347cf637cda8df
jer-meh <jeremy.megyesi@gmail.com>
2017-09-27 16:36:57 -0700

    Program is now capable of generating fully completed sudoku grid by repeatedly attempting generation until successful. Also compressed section of code in subgrid checker.

--- SudokuGen1.py
+++ SudokuGen1.py
@@ -1,3 +1,4 @@
+
 """
 Generates Sudoku Puzzles on a 9x9 grid
 Numbers 1-9 with no repeating numbers in rows, columns, or sub-grids of 3x3
@@ -6,6 +7,7 @@ Upon generating a complete solution, must delete numbers to create an unambiguou
 
 import sys
 import random
+from time import time
 from SudokuSolver import print_puzzle
 from SudokuSolver import get_cell
 from SudokuSolver import get_row
@@ -13,42 +15,76 @@ from SudokuSolver import get_col
 from SudokuSolver import get_box
 from SudokuSolver import set_cell
 
-#: variables used in multiple functions
-rows = 9
-columns = 9
-new_num = 0
-grid = [[0 for i in range(columns)] for j in range(rows)]
-flag = False
 
 #: primary function where grid is generated
 def generate_grid():	
+	t1 = time()  #: starts timer to measure program speed
+	rows = 9
+	columns = 9
+	new_num = 0  #: number to be added to the grid
 
-	for row_index in range(len(grid)):
-		for column_index in range(len(grid[row_index])):
+	#: create a zero matrix as an empty grid
+	#: imported print_puzzle function will display zeroes as blank space
+	grid = [[0 for i in range(columns)] for j in range(rows)]
+
+	is_stuck = False
+	
+	#: start infinite loop to repeatedly attempt to generate a complete sudoku grid
+	#: breaks when complete grid is generated 
+	while True:
+		
+		#: traverse the grid left to right, top to bottom
+		#: adds random number 1-9 at each location
+		for row_index in range(len(grid)):
+			for column_index in range(len(grid[row_index])):
 			
-			while True:
+				#: create loop to repeatedly attempt to place new number
+				#: checks repetition rules before placing, if repetition -> try again
+				#: limit 999 added in case generator reaches logical stalemate -> try again
+				for i in range(999):
+					
+					#: set flag before reaching end of loop to break all loops and try again
+					if i == 998:
+						is_stuck = True
 
-				new_num = random.randint(1,9)  #: starts with random number 1-9
-				#: if first number in row, only check column
-				if column_index == 0:
-					if check_col(new_num, 0, columns, column_index):
-						if check_group(row_index, column_index, new_num):
-							grid[row_index][column_index] = new_num
-							break
+					new_num = random.randint(1,9)  #: starts with random number 1-9
 
-				#: checks both for row and column
-				#: if number is already in row or column, restart loop with new random number
-				elif new_num not in grid[row_index]:
-					if check_col(new_num, 0, columns, column_index):
-						if check_group(row_index, column_index, new_num):
-							grid[row_index][column_index] = new_num
-							print_puzzle(grid)
-							break
-	print_puzzle(grid)  #: prints grid (imported from SudokuSolver)
+					#: if first number in row, only check column
+					if column_index == 0:
+						if check_col(grid, new_num, 0, columns, column_index):
+							if check_group(grid, row_index, column_index, new_num):
+								grid[row_index][column_index] = new_num
+								break
+
+					#: checks both for row and column
+					#: if number is already in row or column, restart loop with new random number
+					elif new_num not in grid[row_index]:
+						if check_col(grid, new_num, 0, columns, column_index):
+							if check_group(grid, row_index, column_index, new_num):
+								grid[row_index][column_index] = new_num
+								break
+				if is_stuck == True:
+					break
+			if is_stuck == True:
+				is_stuck = False
+				break	
+		
+		#: check for complete grid
+		#: if grid has been completely filled out: print time and puzzle, break loop, end program
+		if check_zero(grid):
+			t2 = time()
+			print(str(t2 - t1) + " seconds")
+			print_puzzle(grid)
+			break
+		
+		#: if program reaches this point, puzzle generation was unsuccessful
+		#: reset grid and try again
+		grid = [[0 for i in range(columns)] for j in range(rows)]
+
 
 #: checks if new number is already in the column
 #: returns false if number is already in column
-def check_col(temp, s_cols, e_cols, column_index) -> bool:
+def check_col(grid, temp, s_cols, e_cols, column_index) -> bool:
 	for row_index in range(len(grid)):
 		if row_index == e_cols:
 			break
@@ -58,7 +94,7 @@ def check_col(temp, s_cols, e_cols, column_index) -> bool:
 
 
 #: checks 3x3 sub-grid for repeating numbers
-def check_group(row_index, column_index, x) -> bool:
+def check_group(grid, row_index, column_index, x) -> bool:
 
 	#: start by grouping 3x3 sub-grids
 
@@ -81,80 +117,13 @@ def check_group(row_index, column_index, x) -> bool:
 	elif column_index > 5:
 		gc = 3
 
-	#: fill in subgrid
-	if gr == 1:
+	#: fill in subgrid	
+	for r_i in range(9):
+		for c_i in range(9):
 
-		if gc == 1:
-
-			for r_i in range(3):
-				for c_i in range(3):
-					subgrid[r_i][c_i] = grid[r_i][c_i]
-	
-		if gc == 2:
-
-			for r_i in range(3):
-				for c_i in range(6):
-
-					if c_i > 2:
-						subgrid[r_i][c_i-3] = grid[r_i][c_i]
-		
-		if gc == 3:
-
-			for r_i in range(3):
-				for c_i in range(9):
-				
-					if c_i > 5:
-						subgrid[r_i][c_i-6] = grid[r_i][c_i]
-	if gr == 2:
-
-		if gc == 1:
-
-			for r_i in range(6):
-				for c_i in range(3):
-
-					if r_i > 2:
-						subgrid[r_i-3][c_i] = grid[r_i][c_i]
-	
-		if gc == 2:
-
-			for r_i in range(6):
-				for c_i in range(6):
-
-					if r_i > 2 and c_i > 2:
-						subgrid[r_i-3][c_i-3] = grid[r_i][c_i]
-		
-		if gc == 3:
-
-			for r_i in range(6):
-				for c_i in range(9):
-				
-					if r_i > 2 and c_i > 5:
-						subgrid[r_i-3][c_i-6] = grid[r_i][c_i]
-	if gr == 3:
-
-		if gc == 1:
-
-			for r_i in range(9):
-				for c_i in range(3):
-
-					if r_i > 5:
-						subgrid[r_i-6][c_i] = grid[r_i][c_i]
-	
-		if gc == 2:
-
-			for r_i in range(9):
-				for c_i in range(6):
-
-					if r_i > 5 and c_i > 2:
-						subgrid[r_i-6][c_i-3] = grid[r_i][c_i]
-		
-		if gc == 3:
-
-			for r_i in range(9):
-				for c_i in range(9):
-				
-					if r_i > 5 and c_i > 5:
-						subgrid[r_i-6][c_i-6] = grid[r_i][c_i]
+			if r_i > gr*3-3 and r_i < gr*3:  #: row index must be between start of subgrid and end of subgrid
+				if c_i > gc*3-3 and c_i < gc*3:  #: column index must also be between start and end of subgrid
+					subgrid[r_i-(gr*3-3)][c_i-(gc*3-3)] = grid[r_i][c_i]
 
 	#: check to see if number is already in subgrid
 	for i in range(3):
@@ -164,5 +133,14 @@ def check_group(row_index, column_index, x) -> bool:
 	
 	return True
 
+
+def check_zero(grid) -> bool:
+	
+	for row_index in range(len(grid)):
+		for column_index in range(len(grid[row_index])):
+			if grid[row_index][column_index] == 0:
+				return False
+	return True
+ 
 if __name__ == "__main__":
 	generate_grid()
