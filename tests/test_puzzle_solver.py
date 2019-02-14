45f2e767a0e3282ae37370329f93df43801a5f06
Ross Alexandra <ross3834@uvic.ca>
2019-02-14 11:17:33 -0800

    GH-3 - Fixed failing test.

--- tests/test_puzzle_solver.py
+++ tests/test_puzzle_solver.py
@@ -1,7 +1,8 @@
-from sudoku import PuzzleSolver
+from sudoku import Puzzle, PuzzleSolver
+from tests.test_utils import generate_valid_incomplete_puzzle
 
 
 def test_initialize_puzzle_solver():
-    ps = PuzzleSolver()
+    ps = PuzzleSolver(Puzzle(generate_valid_incomplete_puzzle()))
 
     assert isinstance(ps, PuzzleSolver)
